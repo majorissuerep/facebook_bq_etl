48b57524a59b3cbe2928a41fa1ba664001078d77
r-sukach <majorissue@outlook.com>
2020-12-17 20:06:15 +0200

    v2.0

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 from google.cloud import bigquery
 from google.cloud.exceptions import NotFound
 from datetime import datetime, date, timedelta
-import requests
 import logging
-import json
-import base64
 
 from facebook_business.api import FacebookAdsApi
 from facebook_business.adobjects.adaccount import AdAccount
-from facebook_business.adobjects.adaccountuser import AdAccountUser
 from facebook_business.adobjects.adsinsights import AdsInsights
-from facebook_business.adobjects.campaign import Campaign
 
 short_interval_duration = 30
 
@@ -44,7 +39,7 @@ schema_facebook_stat = [
 
 clustering_fields_facebook = ['campaign_id', 'campaign_name']
 
-
+# TODO: CHECK TABLE AND CREATE VIEV
 def check_or_create_dataset(client, project_id, dataset_id):
     try:
         dataset_ref = "{}.{}".format(project_id, dataset_id)
@@ -85,10 +80,6 @@ def create_table(client, project_id, dataset_id, table_id, schema, clustering_fi
     except:
         logger.error("Creating new table went wrong {}".format(table_ref))
         raise
-
-#
-# def read_facebook_api(app_id, app_secret, access_token, account_id, **kwargs):
-#     return [{'a': 1, 'b': 2}, {'a': 12, 'b': 23}]
 
 
 def read_facebook_api(app_id, app_secret, access_token, account_id, **kwargs):
@@ -187,74 +178,14 @@ def long_read_facebook_api(app_id, app_secret, access_token, account_id, **kwarg
                                           until=end_date)
     return all_insights
 
-#
-# def get_facebook_data(event, context):
-#     # bigquery_client = bigquery.Client()
-#     bigquery_client = 'BLYAT'
-#
-#     end_date = (date.today() - timedelta(1))
-#     if 'end_date' in event['attributes']:
-#         end_date = datetime.strptime(event['attributes']['end_date'], '%Y-%m-%d')
-#
-#     dataset_id = event['attributes']['dataset_id']
-#     project_id = event['attributes']['project_id']
-#     table_id = event['attributes']['table_id']
-#
-#     app_id = event['attributes']['app_id']
-#     app_secret = event['attributes']['app_secret']
-#     access_token = event['attributes']['access_token']
-#     account_id = event['attributes']['account_id']
-#
-#     start_date = datetime.strptime(event['attributes']['start_date'], '%Y-%m-%d')
-#
-#     fill_up = False
-#     if 'fill_up' in event['attributes']:
-#         fill_up = True
-#
-#
-#     TIMEEEE = (end_date - end_date).days
-#     if (end_date - start_date).days > short_interval_duration:
-#         long_term_table_id = table_id + '_long'
-#         long_term_table_end_date = end_date - timedelta(short_interval_duration)
-#         if fill_up:
-#             # FILL UP ALL THE TABLE
-#             # check_or_create_dataset(bigquery_client, project_id, dataset_id)
-#             # delete_existing_table(bigquery_client, project_id, dataset_id, long_term_table_id)
-#             # table_obj = create_table(bigquery_client, project_id, dataset_id, long_term_table_id, schema_facebook_stat,
-#             #                          clustering_fields_facebook)
-#             insights = long_read_facebook_api(app_id, app_secret, access_token, account_id, since=start_date,
-#                                               until=long_term_table_end_date)
-#             writable_insights = transform_insights(insights)
-#             # if table_obj:
-#             #     insert_rows_bq_obj(bigquery_client, table_obj, writable_insights)
-#             # else:
-#             #     insert_rows_bq(bigquery_client, long_term_table_id, dataset_id, project_id, writable_insights)
-#         else:
-#             # INSERT ONLY ONE DAY INSTEAD
-#             last_day_date = end_date - timedelta(short_interval_duration)
-#             check_or_create_dataset(bigquery_client, project_id, dataset_id)
-#             insights = read_facebook_api(app_id, app_secret, access_token, account_id, since=last_day_date,
-#                                          until=last_day_date)
-#             writable_insights = transform_insights(insights)
-#             insert_rows_bq(bigquery_client, long_term_table_id, dataset_id, project_id, writable_insights)
-#         # after everything done here, we can rewrite start_date
-#         start_date = end_date - timedelta(short_interval_duration)
-#     short_term_table_id = table_id + '_short'
-#     # Fast google BQ api operations first!!!
-#     check_or_create_dataset(bigquery_client, project_id, dataset_id)
-#     delete_existing_table(bigquery_client, project_id, dataset_id, short_term_table_id)
-#     table_obj = create_table(bigquery_client, project_id, dataset_id, short_term_table_id, schema_facebook_stat,
-#                              clustering_fields_facebook)
-#     # FB API operations
-#     insights = read_facebook_api(app_id, app_secret, access_token, account_id, since=start_date, until=end_date)
-#
-#     writable_insights = transform_insights(insights)
-#     if table_obj:
-#         insert_rows_bq_obj(bigquery_client, table_obj, writable_insights)
-#     else:
-#         insert_rows_bq(bigquery_client, short_term_table_id, dataset_id, project_id, writable_insights)
-#     return 'ok'
 
+def check_table_existance(bigquery_client, project_id, dataset_id, table_id):
+    table_ref = "{}.{}.{}".format(project_id, dataset_id, table_id)
+    try:
+        bigquery_client.get_table(table_ref)  # Make an API request.
+        return True
+    except NotFound:
+        return False
 
 
 def get_facebook_data(event, context):
@@ -275,14 +206,10 @@ def get_facebook_data(event, context):
 
     start_date = datetime.strptime(event['attributes']['start_date'], '%Y-%m-%d')
 
-    fill_up = False
-    if 'fill_up' in event['attributes']:
-        fill_up = True
-
     if (start_date - end_date).days > short_interval_duration:
         long_term_table_id = table_id + '_historical'
         long_term_table_end_date = end_date-timedelta(short_interval_duration)
-        if fill_up:
+        if not check_table_existance(bigquery_client, project_id, dataset_id, long_term_table_id):
             # FILL UP ALL THE TABLE
             check_or_create_dataset(bigquery_client, project_id, dataset_id)
             delete_existing_table(bigquery_client, project_id, dataset_id, long_term_table_id)
@@ -320,20 +247,3 @@ def get_facebook_data(event, context):
     else:
         insert_rows_bq(bigquery_client, short_term_table_id, dataset_id, project_id, writable_insights)
     return 'ok'
-
-if __name__ == '__main__':
-    event = {
-        'attributes': {
-            'end_date': '2020-12-12',
-            'start_date': '2020-06-12',
-            'table_id': 'huinya',
-            'dataset_id': 'blyat',
-            'project_id': 'sdgfsa',
-            'app_id': '123142',
-            'app_secret': '81874',
-            'access_token': '123141',
-            'account_id': '1',
-            'fill_up': ''
-        }
-    }
-    get_facebook_data(event, None)
